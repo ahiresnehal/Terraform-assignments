808349ccc5c0407b85271479cf7286a9de1300f5
ahiresnehal <102793976+ahiresnehal@users.noreply.github.com>
2023-02-14 18:39:30 +0530

    Assignment 1 of terraform

---
+++ main.tf
@@ -0,0 +1,23 @@
+
+# Terraform Settings Block
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      #version = "~> 3.21" # Optional but recommended in production
+    }
+  }
+}
+
+# Provider Block
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = "AKIAUWD4CWWWUQIMBSEY"
+  secret_key = "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"
+}
+
+# Resource Block
+resource "aws_instance" "ec2demo" {
+  ami           = "ami-0f8ca728008ff5af4"
+  instance_type = "t2.micro"
+}
