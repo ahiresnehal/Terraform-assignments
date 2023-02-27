5cd264936d5539b3de0a44231049fd817c33525a
ahiresnehal <102793976+ahiresnehal@users.noreply.github.com>
2023-02-27 18:19:28 +0530

    Update provider.tf

--- for_each/provider.tf
+++ for_each/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIAUWD4CWWW4M5WUUOZ"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
